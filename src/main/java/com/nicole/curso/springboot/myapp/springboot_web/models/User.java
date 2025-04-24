3fa683fe764f687835924e6f626175085e468ee6
Nicole <alvaradonicole123@hotmail.com>
2025-04-24 18:51:15 -0300

    agregado package models y clase User

---
+++ src/main/java/com/nicole/curso/springboot/myapp/springboot_web/models/User.java
@@ -0,0 +1,26 @@
+package com.nicole.curso.springboot.myapp.springboot_web.models;
+
+public class User {
+
+    public User(String name, String lastname) {
+        this.name = name;
+        this.lastname = lastname;
+    }
+
+    private String name;
+    private String lastname;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+    
+}

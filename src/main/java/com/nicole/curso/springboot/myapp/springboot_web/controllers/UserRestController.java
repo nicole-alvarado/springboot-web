297876af5d6917a31ddc9beb77b20ba16fb85e51
Nicole <alvaradonicole123@hotmail.com>
2025-04-24 18:47:41 -0300

    agregado prefijo '/api' y comentarios explicativos

--- src/main/java/com/nicole/curso/springboot/myapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/nicole/curso/springboot/myapp/springboot_web/controllers/UserRestController.java
@@ -4,12 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// Esto es API Rest
 @RestController // Convierte el método handler en un método handler Rest
+@RequestMapping("/api")
 public class UserRestController {
     
-    @GetMapping("/details2")
+    @GetMapping("/details")
     public Map<String,Object> details(){
         Map<String,Object> body = new HashMap<>();
 
@@ -20,3 +23,6 @@ public class UserRestController {
     }
 
 }
+
+// Nota: También podría en lugar de @RestController usar @Controller, y debajo del @GetMapping usar @ResponseBody, ya que esta forma separada es equivalente a @RestController
+// GetMapping es un alias, un atajo, es equivalente a: @RequestMapping(path = "/details", method = RequestMethod.GET)

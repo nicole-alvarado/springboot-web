516ed56fa7305214e324ca2a4d2f49b0a873edb1
Nicole <alvaradonicole123@hotmail.com>
2025-05-03 19:57:10 -0300

    Agregar atributo secondName

--- src/main/java/com/nicole/curso/springboot/myapp/springboot_web/models/User.java
+++ src/main/java/com/nicole/curso/springboot/myapp/springboot_web/models/User.java
@@ -3,10 +3,12 @@ package com.nicole.curso.springboot.myapp.springboot_web.models;
 public class User {
 
     private String name;
+    private String secondName;
     private String lastname;
     
-    public User(String name, String lastname) {
+    public User(String name, String secondName, String lastname) {
         this.name = name;
+        this.secondName = secondName;
         this.lastname = lastname;
     }
 
@@ -15,6 +17,12 @@ public class User {
     }
     public void setName(String name) {
         this.name = name;
+    }
+    public String getSecondName() {
+        return secondName;
+    }
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
     }
     public String getLastname() {
         return lastname;

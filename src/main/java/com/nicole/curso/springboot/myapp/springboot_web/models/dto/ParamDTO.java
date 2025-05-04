608f956b8c01342839d640d818488f8f677d3bac
Nicole <alvaradonicole123@hotmail.com>
2025-05-03 21:22:46 -0300

    Crear ParamDTO para request de paŕámetros

---
+++ src/main/java/com/nicole/curso/springboot/myapp/springboot_web/models/dto/ParamDTO.java
@@ -0,0 +1,16 @@
+package com.nicole.curso.springboot.myapp.springboot_web.models.dto;
+
+public class ParamDTO {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    
+}

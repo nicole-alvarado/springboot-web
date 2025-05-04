49bfd569e5a89f081504616504ebb07e30ebe544
Nicole <alvaradonicole123@hotmail.com>
2025-05-03 21:30:42 -0300

    Agregar atributo code

--- src/main/java/com/nicole/curso/springboot/myapp/springboot_web/models/dto/ParamDTO.java
+++ src/main/java/com/nicole/curso/springboot/myapp/springboot_web/models/dto/ParamDTO.java
@@ -3,6 +3,7 @@ package com.nicole.curso.springboot.myapp.springboot_web.models.dto;
 public class ParamDTO {
 
     private String message;
+    private Integer code;
 
     public String getMessage() {
         return message;
@@ -10,6 +11,14 @@ public class ParamDTO {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
     }
 
     
